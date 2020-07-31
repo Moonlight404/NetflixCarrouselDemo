189e62e212d8c690cf9ccb5decadd865b29b5be3
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-07-30 22:25:22 -0300

    Demo view

---
+++ precache-manifest.4becdd917b58e7becffe7dc066ac5abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72aa6c7c20cf21f56fb1d42da8c18f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23257adaffae243d1d50",
+    "url": "/static/css/main.5a8bc703.chunk.css"
+  },
+  {
+    "revision": "46d9feae2c99ebb1d652",
+    "url": "/static/js/2.82b5dca2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82b5dca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23257adaffae243d1d50",
+    "url": "/static/js/main.1ed63f9f.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  }
+]);
